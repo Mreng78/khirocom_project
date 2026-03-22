662fd5a9fea93a9316331aa2a7a863c9f653074c
Mreng78 <mreng400@gmail.com>
2026-03-22 18:57:18 +0300

    controoler of area has been ceated

--- backend/src/routes/areaRouts.js
+++ backend/src/routes/areaRouts.js
@@ -5,14 +5,14 @@ const auth = require('../middleware/auth');
 const areacontroller=require('../controllers/AreaController')
 
 
-router.get('/getallareas',areacontroller.getallareas)
-router.get('/getareabyid',areacontroller.getareaById)
-router.post('/addarea',areacontroller.addarea)
-router.put('/updatearea',areacontroller.updatearea)
-router.delete('/deletearea',areacontroller.deletearea)
-router.put('/updatearea',areacontroller.updatearea)
-router.get('/getareasbysupervisor',areacontroller.getareaBySupervisor)
-router.get('/getareasbymentor',areacontroller.getareaByMentor)
-router.get('/getallstudentscount',areacontroller.getallstudentscount)
+router.get('/getallareas',auth,areacontroller.getallareas)
+router.get('/getareabyid',auth,areacontroller.getareaById)
+router.post('/addarea',auth,areacontroller.addarea)
+router.put('/updatearea',auth,areacontroller.updatearea)
+router.delete('/deletearea',auth,areacontroller.deletearea)
+router.put('/updatearea',auth,areacontroller.updatearea)
+router.get('/getareasbysupervisor',auth,areacontroller.getareaBySupervisor)
+router.get('/getareasbymentor',auth,areacontroller.getareaByMentor)
+router.get('/getallstudentscount',auth,areacontroller.getallstudentscount)
 
 module.exports = router;
